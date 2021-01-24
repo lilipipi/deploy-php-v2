0b0ef0c552f6d8d72c0ef10567fcb059cd51d40f
Hung <s3715272@rmit.edu.vn>
2021-01-24 22:37:06 +1100

    initial commit

---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+<!-- Footer section start -->
+<div class="footer"> 
+        <div class="wrapper">
+       <p class="text-center">Quy Buu Restaurant. <a href="#"></a></p>
+        </div>
+    </div>
+    <!-- Footer section end -->
+</body>
+</html>
---
+++ config/constants.php
@@ -0,0 +1,25 @@
+<?php 
+
+// Star Session
+session_start();
+
+define('SITEURL','http://localhost/food-order/');
+define('LOCALHOST','us-cdbr-east-03.cleardb.com');
+define('DB_USERNAME','b1839f3c805a40');
+define('DB_PASSWORD','5007c482');
+define('DB_NAME','food-order');
+
+
+$conn = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die(mysqli_error());
+$db_select= mysqli_select_db ($conn ,DB_NAME) or die(mysqli_error());
+
+//mysql://b1839f3c805a40:5007c482@us-cdbr-east-03.cleardb.com/heroku_c7ee9cbbc0034f6?reconnect=true
+
+
+
+
+
+
+
+
+?>
