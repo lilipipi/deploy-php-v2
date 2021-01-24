75f115cec520d982b0cb70953fda61b40c51ee22
Hung <s3715272@rmit.edu.vn>
2021-01-24 22:45:09 +1100

    fixed constants

--- config/constants.php
+++ config/constants.php
@@ -3,17 +3,17 @@
 // Star Session
 session_start();
 
-define('SITEURL','https://obscure-beyond-30372.herokuapp.com/food-order/');
+define('SITEURL','https://damp-everglades-25724.herokuapp.com/food-order/');
 define('LOCALHOST','us-cdbr-east-03.cleardb.com');
-define('DB_USERNAME','b1839f3c805a40');
-define('DB_PASSWORD','5007c482');
+define('DB_USERNAME','b992cf6ba413a1');
+define('DB_PASSWORD','a554eee6');
 define('DB_NAME','food-order');
 
 
 $conn = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die(mysqli_error());
 $db_select= mysqli_select_db ($conn ,DB_NAME) or die(mysqli_error());
 
-//mysql://b1839f3c805a40:5007c482@us-cdbr-east-03.cleardb.com/heroku_c7ee9cbbc0034f6?reconnect=true
+//mysql://b992cf6ba413a1:a554eee6@us-cdbr-east-03.cleardb.com/heroku_d4d9e18c3dc7551?reconnect=true
 
 
 
